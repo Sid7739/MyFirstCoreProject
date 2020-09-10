a6ae18889cc5710cbfeb050e22e33a94126566ae
Sidhant Das <sidhant4@live.com>
2020-09-11 04:20:42 +0530

    Commit Changes

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstMvc_CoreApplicant.Models;
+using FirstMvc_CoreApplicant.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvc_CoreApplicant.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository =null;
+
+        public BookController( )
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public List<BookModel> GetAllBook()
+        {
+            return _bookRepository.GetAllBook();
+        }
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string name,string author)
+        {
+            return _bookRepository.SearchBook(name, author);
+        }
+    }
+}
---
+++ Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using FirstMvc_CoreApplicant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvc_CoreApplicant.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBook()
+        {
+            return BookDataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return BookDataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string name,string author)
+        {
+            return BookDataSource().Where(x=>x.Name.Contains(name) || x.Author.Contains(author)).ToList();
+        }
+
+        private List<BookModel> BookDataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1,Name="Love",Author="sidhant"},
+                new BookModel(){Id=2,Name="Love Ajkal",Author="Sravani"},
+                new BookModel(){Id=3,Name="MVC",Author="Rabindra"},
+                new BookModel(){Id=4,Name="Banda",Author="sidhant"},
+                new BookModel(){Id=4,Name="Hello",Author="sidhant Das"},
+            };
+        }
+    }
+}
